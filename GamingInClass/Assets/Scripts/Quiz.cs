1cbcc9dea6cd916698a8fadfcc67e6750024048a
Jozka Siregar <jozkasiregar@gmail.com>
2024-02-06 20:45:01 +0700

    correct answer and wrong answer

--- GamingInClass/Assets/Scripts/Quiz.cs
+++ GamingInClass/Assets/Scripts/Quiz.cs
@@ -8,10 +8,39 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] private QuestionSO question;
     [SerializeField] private TextMeshProUGUI questionText;
+    [SerializeField] private GameObject[] answerButtons;
+    [SerializeField] private Sprite defaultAnswerSprite;
+    [SerializeField] private Sprite correctAnswerSprite;
+
+    int correctAnswerIndex;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
     }
 
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text += "\n Jawaban anda benar!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text += "\n Jawaban anda salah! Jawaban yang benar adalah " + correctAnswer;
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+    }
 }
