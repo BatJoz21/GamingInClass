3ba57ab3dd30847ec47c8aca7acd73dfa450e393
Jozka Siregar <jozkasiregar@gmail.com>
2024-11-08 18:18:46 +0700

    After thesis hearing

--- GamingInClass/Assets/Scripts/QuestionSO.cs
+++ GamingInClass/Assets/Scripts/QuestionSO.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionSO : ScriptableObject
